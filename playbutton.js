705c7ea5e87c04dcc1f96afa728d45950379aed7
4real4real247 <cassy.h702@gmail.com>
2024-11-20 12:03:51 -0800

    had to add a second js.script because my js wasn't working

---
+++ playbutton.js
@@ -0,0 +1,52 @@
+const startstop = document.getElementById("startstop");
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
+const hours = document.getElementById("hours");
+const days = document.getElementById("days");
+
+const today = new Date();
+const todayAt12 = new Date();
+todayAt12.setHours(0, 0, 0, 0);
+const birthdate = new Date("01/08/2025");
+let totalSeconds = 0;
+if (todayAt12.getTime() === birthdate.getTime()) {
+  totalSeconds = 0;
+} else {
+  birthdate.setFullYear(today.getFullYear() + 1);
+  totalSeconds = (birthdate - today) / 1000;
+}
+
+const setDisplay = (sec) => {
+  console.log(day);
+  days.innerText = String(Math.floor(sec / (24 * 60 * 60))).padStart(2, "0");
+  console.log(Math.floor(sec / (24 * 60 * 60)));
+  sec = sec % (24 * 60 * 60);
+  hours.innerText = String(Math.floor(sec / (60 * 60))).padStart(2, "0");
+  sec = sec % (60 * 60);
+  minutes.innerText = String(Math.floor(sec / 60)).padStart(2, "0");
+  sec = sec % 60;
+  seconds.innerText = String(Math.floor(sec)).padStart(2, "0");
+};
+
+const timer = setInterval(() => {
+  if (totalSeconds <= 0) {
+    totalSeconds = 0;
+    clearInterval(timer);
+    return;
+  }
+  totalSeconds -= 1;
+  setDisplay(totalSeconds);
+}, 1000);
+
+const audio = document.getElementById("myAudio");
+const playButton = document.getElementById("playButton");
+
+playButton.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playButton.textContent = "Pause";
+  } else {
+    audio.pause();
+    playButton.textContent = "Play";
+  }
+});
